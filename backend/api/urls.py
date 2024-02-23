c27a1015017a84fb3546ed6e8b130cf2e57d7981
Harshit Sharma <hepta1000@gmail.com>
2024-02-23 12:44:00 +0530

    Update

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, import_students_from_excel, json_response_view
+from .views import index, import_students_from_excel, json_response_view, CBV_Example
 # here we map our view to the api endpoint
 # what view will respond to a particular request or endpoint
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('',index, name="Index"),
     path('impot_data/', import_students_from_excel, name="Import Data"),
     path('return-json-response/', json_response_view, name="Json response"),
+    path('CBV-Example/', CBV_Example.as_view(), name="CBV Example"),
 ]
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.forms.models import model_to_dict
 import openpyxl
 from .models import Student
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 # Create your views here.
 # basically responses to our requests
 
@@ -9,18 +13,68 @@ def index(request):
     return HttpResponse("Welcome to my api backend")
 
 # json response example
+@api_view(['GET'])
 def json_response_view(request):
-    data= {
-        'message':'hello, this is json response',
-        'status':'success',
-        'data':{
-            'name':'Alex',
-            'major':'Physics',
-            'age': 167
-        }
-    }
 
-    return JsonResponse(data)
+    # data = {
+    #     'message' : 'hello, this is a JSON Response',
+    #     'status' : 'success',
+    #     'data' : {
+    #         'name' : 'Alex',
+    #         'major' : 'Physics',
+    #         'age' : 167
+    #     }
+    # }
+
+
+    # data = {}
+    single_student = Student.objects.all().order_by("?").first()
+    
+
+    # if single_student:
+    #     data['id'] = single_student.id
+    #     data['first_name'] = single_student.first_name
+    #     data['last_name'] = single_student.last_name
+    #     data['age'] = single_student.age
+    #     data['major'] = single_student.major
+
+    data = model_to_dict(single_student, 
+                         fields=['id', 'first_name','last_name'])
+
+    return Response(data)
+
+
+# Class based view example
+class CBV_Example(APIView):
+    def get(self, request):
+        data = {
+            'message' : 'This is a CBV Get response'
+        }
+        return Response(data)
+    
+    def post(self, request):
+        data = {
+            'message' : 'This is a CBV Post response'
+        }
+        return Response(data)
+    
+    def put(self, request):
+        data = {
+            'message' : 'This is a CBV Put response'
+        }
+        return Response(data)
+    
+    def patch(self, request):
+        data={
+            'message':'This is a CBV Patch response'
+        }
+        return Response(data)
+    
+    def delete(self, request):
+        data={
+            'message':'This is a CBV Delete response'
+        }
+        return Response(data)
 
 
 # import data
